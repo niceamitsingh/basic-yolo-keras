3dbdf958daa724dbe26d32c7ba4e6243a0cf2619
Amit Singh <amit18.singh@ril.com>
2019-06-21 11:53:23 +0530

    Added page

--- src/index.js
+++ src/index.js
@@ -3,9 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ProducerDashboard from './screens/producer.dashboard';
 
+const routing = (
+    <Router>
+        <div>
+            <ul>
+
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+
+                <li>
+                    <Link to="/producer">producer</Link>
+                </li>
+            </ul>
+            <Route exact path="/" component={App} />
+            <Route path="/producer" component={ProducerDashboard} />
+        </div>
+    </Router>
+)
+ReactDOM.render(routing, document.getElementById('root'))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
